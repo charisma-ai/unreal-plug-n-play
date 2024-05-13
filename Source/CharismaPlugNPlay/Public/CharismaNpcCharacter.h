0c6cc3c2f1ac1d2df8fadc465c55d3dd29a8aa21
Rob G <robertgesicki@gmail.com>
2024-05-13 12:43:37 +0100

    Add Npc/Character inherited C++ classes, modify game mode to use them

---
+++ Source/CharismaPlugNPlay/Public/CharismaNpcCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharismaCharacterBase.h"
+#include "CharismaNpcCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARISMAPLUGNPLAY_API ACharismaNpcCharacter : public ACharismaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CharismaPlugNPlay/Public/CharismaPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharismaCharacterBase.h"
+#include "CharismaPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARISMAPLUGNPLAY_API ACharismaPlayerCharacter : public ACharismaCharacterBase
+{
+	GENERATED_BODY()
+	
+};
